b691d6d95ce4216afe9758f79d99e06a233fe60c
Darkunitx <darkunitx@live.com>
2023-02-24 22:48:19 -0600

    created schema tables, added node_modules to gitignore and mysql connect to db

---
+++ server.js
@@ -0,0 +1,28 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+const express = require('express');
+const consoleTable = require('console.table');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: '',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the movies_db database.`)
+  );
+
+  
